f09b7b0e476c6c96189626556e7ce51e654d6302
Camela Devs <camelaputri1@gmail.com>
2024-01-24 00:03:54 +0700

    h-1 & h-2

---
+++ javascript/1-multiply.js
@@ -0,0 +1,16 @@
+// SOAL
+
+// This code does not execute properly. Try to figure out why.
+// =======================================
+// function multiply (a, b) {
+//     a * b
+// }
+// =======================================
+
+// JAWAB
+
+function multiply (a, b) {
+    return a * b
+}
+
+console.log (multiply(1, 1));
